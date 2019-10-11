a374f4d07a9e2cc70b28e6d6d4747d44d85ab9fe
Sean Birmingham <39016563+SBirmingham98@users.noreply.github.com>
2019-10-11 01:31:57 -0400

    finished lambda-classes.js, reached MVP

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -6,7 +6,7 @@ class Person {
     }
 
     speak() {
-        console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}.`);
     }
 }
 
@@ -18,10 +18,10 @@ class Instructor extends Person {
         this.catchPhrase = instructorAttr.catchPhrase;
     }
     demo(subject) {
-        console.log(`Today we are learning about ${subject}`);
+        console.log(`Today we are learning about ${subject}.`);
     }
     grade(student, subject) {
-        console.log(`${student.name} receives a perfect score on ${subject}`);
+        console.log(`${student.name} receives a perfect score on ${subject}.`);
     }
 }
 
@@ -32,28 +32,135 @@ class Student extends Person {
         this.className = studentAttr.className;
         this.favSubjects = studentAttr.favSubjects;
     }
-    listSubjects() {
+    listsSubjects() {
         console.log(this.favSubjects);
     }
     PRAssignment(subject) {
-        console.log(`${student.name} has submitted a PR for ${subject}`);
+        console.log(`${this.name} has submitted a PR for ${subject}.`);
     }
     sprintChallenge(subject) {
-        console.log(`${student.name} has begun sprint challenge on ${subject} `);
+        console.log(`${this.name} has begun the sprint challenge on ${subject}.`);
     }
 }
 
 class TeamLead extends Instructor {
     constructor(teamLeadAttr) {
         super(teamLeadAttr);
-        this.gradeClassName = teamLeadAttr.gradeClassName;
+        this.gradClassName = teamLeadAttr.gradClassName;
         this.favInstructor = teamLeadAttr.favInstructor;
     }
 
     standUp(channel) {
-        console.log(`${this.name} announces to ${channel}, @channel standy times!`)
+        console.log(`${this.name} announces to ${channel}, @channel standy times!.`)
     }
-    debugsCode(subject) {
-        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+    debugsCode(student, subject) {
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}.`);
     }
 }
+
+// Instructor 1
+const wayne = new Instructor({
+    name: 'Wayne',
+    age: 29,
+    location: 'Chicago',
+    specialty: 'Front-end',
+    favLanguage: 'JavaScript',
+    catchPhrase: 'Party On, Garth!'
+});
+
+// Instructor 2
+const garth = new Instructor({
+    name: 'Garth',
+    age: 37,
+    location: 'Chicago',
+    specialty: 'Back-end',
+    favLanguage: 'Java',
+    catchPhrase: 'Party On, Wayne!'
+})
+
+// Student 1
+const sean = new Student({
+    name: 'Sean',
+    age: 21,
+    location: 'Maryland',
+    previousBackground: 'High School',
+    className: 'WebPT11',
+    favSubjects: ['HTML', 'CSS', 'JavaScript']
+});
+
+// Student 2
+const chris = new Student({
+    name: 'Chris',
+    age: 25,
+    location: 'New York',
+    previousBackground: 'Sales',
+    className: 'WebPT11',
+    favSubjects: ['HTML', 'CSS']
+});
+
+// Team Lead 1 
+const johnny = new TeamLead({
+    name: 'Johnny',
+    age: 30,
+    location: 'Colorado',
+    specialty: 'Redux',
+    favLanguage: 'React',
+    catchPhrase: `Here's Johnny!`,
+    gradClassName: 'Web23',
+    favInstructor: 'Wayne'
+});
+
+// Team Lead 2
+const joker = new TeamLead({
+    name: 'Joker',
+    age: 28,
+    location: 'Gotham City',
+    specialty: 'clowning around',
+    favLanguage: 'React',
+    catchPhrase: `Why so serious?`,
+    gradClassName: 'Web23',
+    favInstructor: 'Garth'
+});
+
+// Instructor 1
+console.log(wayne);
+wayne.speak();
+console.log(wayne.catchPhrase);
+wayne.demo('JavaScript Classes');
+wayne.grade(sean, 'JavaScript IV');
+
+// Instructor 2
+console.log(garth);
+garth.speak();
+console.log(garth.catchPhrase);
+garth.demo('CSS Flexbox');
+garth.grade(chris, 'User Interface III')
+
+// Student 1
+console.log(sean);
+sean.speak();
+sean.listsSubjects();
+sean.PRAssignment('JavaScript IV');
+sean.sprintChallenge('JavaScript');
+
+// Student 2 
+console.log(chris);
+chris.speak();
+chris.listsSubjects();
+chris.PRAssignment('User Interface III');
+chris.sprintChallenge('User Interface and Git');
+
+// Team Lead 1
+console.log(johnny);
+johnny.speak();
+console.log(johnny.catchPhrase);
+johnny.standUp('WebPT11')
+johnny.debugsCode(sean, 'JavaScript IV');
+
+
+// Team Lead 2
+console.log(joker)
+joker.speak();
+console.log(joker.catchPhrase);
+joker.standUp('WebPT11');
+joker.debugsCode(chris, 'JavaScript III');
